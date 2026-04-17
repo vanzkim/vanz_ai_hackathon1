2e75ea9cfb166b6db4a0d563f997cb99097d8591
supervanz <machinema@gmail.com>
2026-04-17 23:56:48 +0900

    Assistant Message Snapshot 2026-04-17_23-56-47: 제자리 걷기 애니메이션을 활용한 **3인칭 캐릭터 컨트롤러**와 **시네머신 카메라** 설정을 완료했습니다. ### 구현 내용 요약...

--- Assets/Scripts/ThirdPersonController.cs
+++ Assets/Scripts/ThirdPersonController.cs
@@ -4,76 +4,157 @@ using UnityEngine.InputSystem;
 [RequireComponent(typeof(CharacterController))]
 public class ThirdPersonController : MonoBehaviour
 {
-    [Header("Movement")]
-    public float moveSpeed = 5f;
-    public float rotationSpeed = 10f;
-    public float gravity = -9.81f;
+    [Header("Movement Settings")]
+    [SerializeField] private float moveSpeed = 2.0f;
+    [SerializeField] private float rotationSmoothTime = 0.12f;
+    [SerializeField] private float speedChangeRate = 10.0f;
+    [SerializeField] private float gravity = -9.81f;
 
-    [Header("References")]
-    public Transform cameraTransform;
-    private CharacterController controller;
-    private Animator animator;
-    private Vector2 moveInput;
-    private Vector3 velocity;
+    [Header("Cinemachine")]
+    [SerializeField] private GameObject cinemachineCameraTarget;
+    [SerializeField] private float topClamp = 70.0f;
+    [SerializeField] private float bottomClamp = -30.0f;
+
+    // Player state
+    private float _speed;
+    private float _animationBlend;
+    private float _targetRotation = 0.0f;
+    private float _rotationVelocity;
+    private float _verticalVelocity;
+    private float _terminalVelocity = 53.0f;
+
+    // Cinemachine
+    private float _cinemachineTargetYaw;
+    private float _cinemachineTargetPitch;
+
+    // Components
+    private PlayerInput _playerInput;
+    private Animator _animator;
+    private CharacterController _controller;
+    private Camera _mainCamera;
+
+    private Vector2 _move;
+    private Vector2 _look;
+    private bool _isCurrentDeviceMouse;
 
     private void Awake()
     {
-        controller = GetComponent<CharacterController>();
-        animator = GetComponent<Animator>();
-        if (cameraTransform == null && Camera.main != null)
-        {
-            cameraTransform = Camera.main.transform;
-        }
+        _mainCamera = Camera.main;
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
+        _playerInput = GetComponent<PlayerInput>();
     }
 
-    public void OnMove(InputValue value)
+    private void Start()
     {
-        moveInput = value.Get<Vector2>();
-        Debug.Log($"OnMove called: {moveInput}");
+        _cinemachineTargetYaw = cinemachineCameraTarget.transform.rotation.eulerAngles.y;
     }
 
     private void Update()
     {
-        Move();
         ApplyGravity();
+        Move();
+    }
+
+    private void LateUpdate()
+    {
+        CameraRotation();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        _move = value.Get<Vector2>();
+    }
+
+    private void OnLook(InputValue value)
+    {
+        _look = value.Get<Vector2>();
+    }
+
+    private void CameraRotation()
+    {
+        if (_look.sqrMagnitude >= 0.01f)
+        {
+            float deltaTimeMultiplier = _playerInput.currentControlScheme == "Keyboard&Mouse" ? 1.0f : Time.deltaTime;
+
+            _cinemachineTargetYaw += _look.x * deltaTimeMultiplier;
+            _cinemachineTargetPitch += _look.y * deltaTimeMultiplier;
+        }
+
+        // Clamp rotations
+        _cinemachineTargetYaw = ClampAngle(_cinemachineTargetYaw, float.MinValue, float.MaxValue);
+        _cinemachineTargetPitch = ClampAngle(_cinemachineTargetPitch, bottomClamp, topClamp);
+
+        // Update target rotation
+        cinemachineCameraTarget.transform.rotation = Quaternion.Euler(_cinemachineTargetPitch, _cinemachineTargetYaw, 0.0f);
     }
 
     private void Move()
     {
-        Vector3 direction = new Vector3(moveInput.x, 0, moveInput.y);
+        float targetSpeed = _move == Vector2.zero ? 0.0f : moveSpeed;
+        float currentHorizontalSpeed = new Vector3(_controller.velocity.x, 0.0f, _controller.velocity.z).magnitude;
 
-        if (direction.magnitude >= 0.1f)
+        float speedOffset = 0.1f;
+        float inputMagnitude = _move.magnitude;
+
+        // Accelerate or decelerate
+        if (currentHorizontalSpeed < targetSpeed - speedOffset || currentHorizontalSpeed > targetSpeed + speedOffset)
         {
-            // Calculate movement direction relative to camera
-            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + (cameraTransform != null ? cameraTransform.eulerAngles.y : 0);
-            float angle = Mathf.LerpAngle(transform.eulerAngles.y, targetAngle, Time.deltaTime * rotationSpeed);
-            transform.rotation = Quaternion.Euler(0, angle, 0);
-
-            Vector3 moveDir = Quaternion.Euler(0, targetAngle, 0) * Vector3.forward;
-            controller.Move(moveDir.normalized * moveSpeed * Time.deltaTime);
-
-            if (animator != null)
-            {
-                animator.SetFloat("Speed", direction.magnitude);
-            }
+            _speed = Mathf.Lerp(currentHorizontalSpeed, targetSpeed * inputMagnitude, Time.deltaTime * speedChangeRate);
+            _speed = Mathf.Round(_speed * 1000f) / 1000f;
         }
         else
         {
-            if (animator != null)
-            {
-                animator.SetFloat("Speed", 0f);
-            }
+            _speed = targetSpeed;
+        }
+
+        _animationBlend = Mathf.Lerp(_animationBlend, targetSpeed, Time.deltaTime * speedChangeRate);
+        if (_animationBlend < 0.01f) _animationBlend = 0f;
+
+        // Normalise input direction
+        Vector3 inputDirection = new Vector3(_move.x, 0.0f, _move.y).normalized;
+
+        if (_move != Vector2.zero)
+        {
+            _targetRotation = Mathf.Atan2(inputDirection.x, inputDirection.z) * Mathf.Rad2Deg + _mainCamera.transform.eulerAngles.y;
+            float rotation = Mathf.SmoothDampAngle(transform.eulerAngles.y, _targetRotation, ref _rotationVelocity, rotationSmoothTime);
+
+            // Rotate to face input direction relative to camera
+            transform.rotation = Quaternion.Euler(0.0f, rotation, 0.0f);
+        }
+
+        Vector3 targetDirection = Quaternion.Euler(0.0f, _targetRotation, 0.0f) * Vector3.forward;
+
+        // Move the player
+        _controller.Move(targetDirection.normalized * (_speed * Time.deltaTime) + new Vector3(0.0f, _verticalVelocity, 0.0f) * Time.deltaTime);
+
+        // Update animator
+        if (_animator != null)
+        {
+            _animator.SetFloat("Speed", _animationBlend);
         }
     }
 
     private void ApplyGravity()
     {
-        if (controller.isGrounded && velocity.y < 0)
+        if (_controller.isGrounded)
         {
-            velocity.y = -2f;
+            if (_verticalVelocity < 0.0f)
+            {
+                _verticalVelocity = -2f;
+            }
         }
 
-        velocity.y += gravity * Time.deltaTime;
-        controller.Move(velocity * Time.deltaTime);
+        if (_verticalVelocity < _terminalVelocity)
+        {
+            _verticalVelocity += gravity * Time.deltaTime;
+        }
+    }
+
+    private static float ClampAngle(float lfAngle, float lfMin, float lfMax)
+    {
+        if (lfAngle < -360f) lfAngle += 360f;
+        if (lfAngle > 360f) lfAngle -= 360f;
+        return Mathf.Clamp(lfAngle, lfMin, lfMax);
     }
 }
